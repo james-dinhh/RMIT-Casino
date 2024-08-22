7a9f46863823e701cd19f7e2454ebae0be8e9c63
james-dinhh <jamesdinh.bib@gmail.com>
2024-08-22 10:46:49 +0700

    Initial Commit

---
+++ RMIT Casino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RMIT Casino
+//
+//  Created by Dinh Le Hong Tin on 22/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RMIT Casino/RMIT_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_CasinoApp.swift
+//  RMIT Casino
+//
+//  Created by Dinh Le Hong Tin on 22/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
